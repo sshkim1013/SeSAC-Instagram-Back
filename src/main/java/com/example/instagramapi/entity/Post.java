b17850c92db23e281cd883b3c79186820e2a23be
sshkim1013 <sshkim1013@naver.com>
2025-12-10 10:16:28 +0900

    1210 User 엔티티 정의

---
+++ src/main/java/com/example/instagramapi/entity/Post.java
@@ -0,0 +1,42 @@
+package com.example.instagramapi.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@NoArgsConstructor
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2000)
+    private String content;
+
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // 지연로딩 설정
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Post(String content, String imageUrl, User user) {
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.user = user;
+    }
+
+}
