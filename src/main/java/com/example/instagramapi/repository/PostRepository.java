e8e7319c01c05eb22d6ce6f7834f307224f9b45e
sshkim1013 <sshkim1013@naver.com>
2025-12-11 11:32:37 +0900

    1211 feed 기능 구현

--- src/main/java/com/example/instagramapi/repository/PostRepository.java
+++ src/main/java/com/example/instagramapi/repository/PostRepository.java
@@ -3,6 +3,8 @@ package com.example.instagramapi.repository;
 import com.example.instagramapi.entity.Post;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -32,5 +34,13 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     // 사용자 별 게시물 수
     long countByUserId(Long userId);
+
+    // 탐색
+    @Query("SELECT p FROM Post p JOIN FETCH p.user ORDER BY p.createdAt DESC")
+    Slice<Post> findAllWithUserPaging();
+
+    // 피드(내 게시물 + 팔로잉 게시물)
+    @Query("SELECT p FROM Post p JOIN FETCH p.user WHERE p.user.id IN :userIds ORDER BY p.createdAt DESC")
+    Slice<Post> findByUserIdsWithUserPaging(@Param("userIds") List<Long> userIds, Pageable pageable);
 
 }
